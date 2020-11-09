88b3818b36b6383ebc92a05ce34f0ebe3c3b26b9
josueJitzu <iosue27@gmail.com>
2020-11-09 08:41:23 -0600

    nuevo Oso regalos

--- funciones.js
+++ funciones.js
@@ -39,17 +39,17 @@ function darClick() {
     {
         console.debug("Dando click en Iphone");
        //Comentar esto en Debug porque genera muchas ventanas
-         document.getElementById("botonIos").click();
+        // document.getElementById("botonIos").click();
 
     }else if(esAndroid){
         console.debug("Dando click en Android");
        //Comentar esto en Debug porque genera muchas ventanas
        //Cada documento Index tiene sus links correspondientes
-      document.getElementById("botonAndroid").click();
+       document.getElementById("botonAndroid").click();
        
     }else{
         console.debug("No se puede cargar");
-
+       // document.getElementById("botonHome").click();
 
     }
 
